8c6bad1d4131cc606420fd35b942dd85c6cc44e3
Reonin <coreyajeffers@gmail.com>
2018-05-10 09:43:16 -0400

    First commit

---
+++ js/congress.js
@@ -0,0 +1,7 @@
+// fetch('https://api.propublica.org/congress/v1/?congress=115')
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(myJson) {
+//     console.log(myJson);
+//   });
